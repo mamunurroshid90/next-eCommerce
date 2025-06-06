b8e661eee53fe7950f07bf9bd139c67f8cbe5fb0
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2025-06-06 18:36:02 +0600

    added cart summary part

--- src/components/cartPage/CartSummary.tsx
+++ src/components/cartPage/CartSummary.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Title from "../Title";
+import PriceFormate from "../PriceFormate";
 
 const CartSummary = () => {
-  return <div>CartSummary</div>;
+  return (
+    <div className="bg-gray-100 rounded-lg px-4 py-6 sm:p-10 lg:col-span-5 mt-10 lg:mt-0">
+      <Title>Cart Summary</Title>
+      <div>
+        <div>
+          <Title className="text-lg font-medium">Sub Total</Title>
+          <PriceFormate amount={100}></PriceFormate>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartSummary;
